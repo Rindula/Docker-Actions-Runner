9654f1da6661278009e95de1db6bef41bbc32b62
Sven Nolting <rindula@users.noreply.github.com>
2021-03-07 05:06:33 +0100

    fix: request for token

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
 echo "Requesting registration URL at '${registration_url}'"
 
-payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
+payload=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: Bearer ${GITHUB_PAT}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
 ./config.sh \
