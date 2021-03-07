2f22db461fd7f2117df68a8327462e156baf8ae5
Sven Nolting <Rindula@users.noreply.github.com>
2021-03-07 18:50:53 +0100

    chore: debug payload

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Requesting registration URL at '${registration_url}'"
 payload=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: Bearer ${GITHUB_PAT}" ${registration_url})
 RUNNER_TOKEN=$(echo "$payload" | jq .token --raw-output)
 
-echo "$(hostname) - ${RUNNER_TOKEN} - ${GITHUB_OWNER}/${GITHUB_REPOSITORY}"
+echo "$(hostname) - $payload - ${RUNNER_TOKEN} - ${GITHUB_OWNER}/${GITHUB_REPOSITORY}"
 ./config.sh --name "$(hostname)" --token "${RUNNER_TOKEN}" --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} --work /home/github/_work --unattended --replace
 
 remove() {
